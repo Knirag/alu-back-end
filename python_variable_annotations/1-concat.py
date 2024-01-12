39ad6751a3842e895adf39d347693469968dc2bb
Knirag <105249761+Knirag@users.noreply.github.com>
2024-01-12 15:19:43 +0200

    ...

---
+++ python_variable_annotations/1-concat.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+def concat(str1: str, str2: str) -> str:
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+      - str1 (str): The first string to be concatenated.
+      - str2 (str): The second string to be concatenated.
+
+    Returns:
+      - str: The concatenated string of str1 and str2.
+
+    Example:
+      >>> concat("egg", "shell")
+      'eggshell'
+    """
+    return str1 + str2
+
