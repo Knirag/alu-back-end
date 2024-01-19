8b319fd3946916170343e1a118042fd2ae69cf11
Knirag <105249761+Knirag@users.noreply.github.com>
2024-01-19 23:16:34 +0200

    ...

--- pagination/3-hypermedia_del_pagination.py
+++ pagination/3-hypermedia_del_pagination.py
@@ -33,44 +33,44 @@ class BabyNameServer:
             self.__indexed_dataset = {i: dataset[i] for i in range(len(dataset))}
         return self.__indexed_dataset
 
-def get_hyper_index(self, current_index: int = None, page_size: int = 10) -> Dict:
-    """
-    Get the hyper index.
+    def get_hyper_index(self, current_index, page_size) -> Dict:
+        """
+        Get the hyper index.
 
-    Args:
-        current_index: Current index.
-        page_size: Total size of the page.
+        Args:
+            current_index: Current index.
+            page_size: Total size of the page.
 
-    Return:
-        Hyper index.
-    """
-    result_dataset = []
-    index_data = self.indexed_dataset()
-    keys_list = list(index_data.keys())
-    assert current_index + page_size < len(keys_list)
-    assert current_index < len(keys_list)
+        Return:
+            Hyper index.
+        """
+        result_dataset = []
+        index_data = self.indexed_dataset()
+        keys_list = list(index_data.keys())
+        assert current_index + page_size < len(keys_list)
+        assert current_index < len(keys_list)
 
-    if current_index not in index_data:
-        start_index = keys_list[current_index]
-    else:
-        start_index = current_index
-
-    for i in range(start_index, start_index + page_size):
-        if i not in index_data:
-            result_dataset.append(index_data[keys_list[i]])
+        if current_index not in index_data:
+            start_index = keys_list[current_index]
         else:
-            result_dataset.append(index_data[i])
+            start_index = current_index
 
-    next_index: int = current_index + page_size
+        for i in range(start_index, start_index + page_size):
+            if i not in index_data:
+                result_dataset.append(index_data[keys_list[i]])
+            else:
+                result_dataset.append(index_data[i])
 
-    if current_index in keys_list:
-        next_index
-    else:
-        next_index = keys_list[next_index]
+        next_index: int = current_index + page_size
 
-    return {
-        'index': current_index,
-        'next_index': next_index,
-        'page_size': len(result_dataset),
-        'data': result_dataset
-    }
+        if current_index in keys_list:
+            next_index
+        else:
+            next_index = keys_list[next_index]
+
+        return {
+            'index': current_index,
+            'next_index': next_index,
+            'page_size': len(result_dataset),
+            'data': result_dataset
+        }
