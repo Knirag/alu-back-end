b40768d8977dad51b6106a2a1cd1aa8c7132eb7a
Knirag <k.niragire@alustudent.com>
2024-02-17 20:13:59 +0000

    ...

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -27,7 +27,7 @@ def test_access_nested_map(self, nested_map, path, expected):
     ({"a": 1}, ("a", "b"))
 ])
 def test_access_nested_map_exception(self, nested_map, path):
-    """ Test exception handling in access_nested_map """
+    """ Test exception """
     with self.assertRaises(KeyError):
         access_nested_map(nested_map, path)
 
