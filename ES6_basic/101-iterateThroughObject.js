c10b18675c62e2fd66430d8b28d2790688426b81
Knirag <105249761+Knirag@users.noreply.github.com>
2024-03-23 22:55:23 +0200

    ..

--- ES6_basic/101-iterateThroughObject.js
+++ ES6_basic/101-iterateThroughObject.js
@@ -1,11 +1,3 @@
 export default function iterateThroughObject(reportWithIterator) {
-  let employeeNames = ""; // Initialize an empty string to store employee names
-  let currentEmployee;
-
-  while (!(currentEmployee = reportWithIterator.next()).done) {
-    employeeNames += `${currentEmployee.value.employee} | `; // Add employee name with separator
-  }
-
-  // Remove the trailing separator for a cleaner output
-  return employeeNames.slice(0, -2);
+  return reportWithIterator.join(" | ");
 }
