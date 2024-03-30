816704ad119b50f2a6927da5911cc6ff32714cd9
Knirag <105249761+Knirag@users.noreply.github.com>
2024-03-30 17:02:58 +0200

    ...

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,9 +1,9 @@
-import Car from "./10-car.js";
+import Car from './10-car;
 
 class EVCar extends Car {
   constructor(brand, motor, color, range) {
-    super(brand, motor, color); // Call the parent constructor
-    this._range = range; // Store range as a private property
+    super(brand, motor, color); 
+    this._range = range;
   }
 
   get range() {
