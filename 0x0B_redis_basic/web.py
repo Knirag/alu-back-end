54ecb91647b6d4c24b44a697c2c6e283f8619b96
Knirag <105249761+Knirag@users.noreply.github.com>
2024-04-23 22:12:08 +0200

    ....

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -22,7 +22,7 @@ def cache_data(method: Callable) -> Callable:
     """
 
     @wraps(method)
-    def wrapper(url: str) -> str:
+    def invoker(url) -> str:
         """
         Inner wrapper function that handles caching logic and invocation
         of the wrapped function.
@@ -31,16 +31,17 @@ def cache_data(method: Callable) -> Callable:
         Redis key and caches the retrieved data for a configurable time period.
         """
         redis_store.incr(f"count:{url}")
-        cached_result = redis_store.get(f"result:{url}")
-        if cached_result:
-            return cached_result.decode("utf-8")
+        redis_store.incr(f'count:{url}')
+        result = redis_store.get(f'result:{url}')
+        if result:
+            return result.decode('utf-8')
 
         result = method(url)
         redis_store.set(f"count:{url}", 0)
         redis_store.setex(f"result:{url}", 10, result)
         return result
 
-    return wrapper
+    return invoker
 
 
 @cache_data
