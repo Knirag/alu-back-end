daf2f7ee9403abcc47502343a3bca59cf42de493
Knirag <105249761+Knirag@users.noreply.github.com>
2024-04-23 21:18:22 +0200

    ...

--- 0x0B_redis_basic/web.py
+++ 0x0B_redis_basic/web.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-'''A module with tools for request caching and tracking.
-'''
+"""
+This module provides utilities for data caching and request tracking using Redis.
+"""
+
 import redis
 import requests
 from functools import wraps
@@ -8,31 +10,46 @@ from typing import Callable
 
 
 redis_store = redis.Redis()
-'''The module-level Redis instance.
-'''
+"""
+A central Redis instance used for data caching and request tracking.
+"""
 
 
-def data_cacher(method: Callable) -> Callable:
-    '''Caches the output of fetched data.
-    '''
+def cache_data(method: Callable) -> Callable:
+    """
+    Decorator function that caches the output of a wrapped function
+    utilizing Redis for efficient data storage and retrieval.
+    """
+
     @wraps(method)
-    def invoker(url) -> str:
-        '''The wrapper function for caching the output.
-        '''
-        redis_store.incr(f'count:{url}')
-        result = redis_store.get(f'result:{url}')
-        if result:
-            return result.decode('utf-8')
+    def wrapper(url: str) -> str:
+        """
+        Inner wrapper function that handles caching logic and invocation
+        of the wrapped function.
+
+        Tracks the number of requests made to a specific URL using a dedicated
+        Redis key and caches the retrieved data for a configurable time period.
+        """
+        redis_store.incr(f"count:{url}")
+        cached_result = redis_store.get(f"result:{url}")
+        if cached_result:
+            return cached_result.decode("utf-8")
+
         result = method(url)
-        redis_store.set(f'count:{url}', 0)
-        redis_store.setex(f'result:{url}', 10, result)
+        redis_store.set(f"count:{url}", 0)
+        redis_store.setex(f"result:{url}", 10, result)
         return result
-    return invoker
+
+    return wrapper
 
 
-@data_cacher
-def get_page(url: str) -> str:
-    '''Returns the content of a URL after caching the request's response,
-    and tracking the request.
-    '''
+@cache_data
+def fetch_page(url: str) -> str:
+    """
+    Fetches the content of a given URL, leverages caching for improved
+    performance, and tracks request count using Redis.
+
+    Returns the content of the URL as a string after potentially retrieving
+    it from the cache or making a fresh request.
+    """
     return requests.get(url).text
