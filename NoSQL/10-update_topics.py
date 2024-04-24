eb105c210b307a205304e3bc3630842988ed5de2
Knirag <105249761+Knirag@users.noreply.github.com>
2024-04-24 16:26:55 +0200

    ....

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -4,6 +4,10 @@
 
 
 def update_topics(mongo_collection, name, topics):
+    """
+    Prototype: def update_topics(mongo_collection, name, topics):
+    Changes all topics of a school document
+    """
 
     mongo_collection.update_many(
         {'name': name},
