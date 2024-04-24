08a159dfe379fbaadc12cad003031fadd9b26066
Knirag <105249761+Knirag@users.noreply.github.com>
2024-04-24 13:36:00 +0200

    ....

--- NoSQL/9-insert_school.py
+++ NoSQL/9-insert_school.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-'''Insert a document in Python
+'''Inserts a new document in a collection based on kwargs
 '''
 
 
 def insert_school(mongo_collection, **kwargs):
-    '''Inserts a new document in a collection.
+    '''Returning the new _id
     '''
     result = mongo_collection.insert_one(kwargs)
     return result.inserted_id
